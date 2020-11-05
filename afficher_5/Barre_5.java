b9b5393b1f5110d7a591f918c110a4d64466c5f4
Zeltemgos <74002830+Zeltemgos@users.noreply.github.com>
2020-11-05 14:32:38 +0100

    Add files via upload

---
+++ afficher_5/Barre_5.java
@@ -0,0 +1,14 @@
+package afficher_5;
+
+public class Barre_5 {
+
+	public static void main(String[] args) {
+		System.out.println(" ____");
+		System.out.println("|");
+		System.out.println("|____");
+		System.out.println("     |");
+		System.out.println(" ____|");
+
+	}
+
+}
