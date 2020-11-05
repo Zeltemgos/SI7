21d635ef68e30b76b9d56a227d618a4ea2bee1d2
Zeltemgos <74002830+Zeltemgos@users.noreply.github.com>
2020-11-05 14:45:46 +0100

    Add files via upload

---
+++ TD4/table5.java
@@ -0,0 +1,16 @@
+package TD4;
+
+public class table5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		int y = 5;
+		
+		for (;i<= 10;i++){
+			System.out.println(i+"*"+y+"="+i*y);
+		}
+
+	}
+
+}
