de0eadd71bee4c8a2a3b16e1b6d751c8f69f1f74
Zeltemgos <74002830+Zeltemgos@users.noreply.github.com>
2020-11-05 14:44:50 +0100

    Add files via upload

---
+++ TD4/Barre_5.java
@@ -0,0 +1,14 @@
+package TD4;
+
+public class Barre_5 {
+
+	public static void main(String[] args) {
+		System.out.println(" ____");
+		System.out.println("|");
+		System.out.println("|____");
+		System.out.println("     |");
+		System.out.println(" ____|");
+
+	}
+
+}
